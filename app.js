a547d0441aace2892a312d657ff151fcdf301748
EDUARDO FEITOSA BATISTA <25132675@CFP127ALUNO.LOCAL>
2025-10-30 15:15:23 -0300

    Header, Main e Footer Finalizados

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const btnRick = document.getElementById('btn_rick')
+const btnEstados = document.getElementById('btn_estados')
+
+btnRick.addEventListener('click', function(){
+    let url = 'https://github.com/EduardoFeitosa08/Rick-and-Morty-Wiki'
+    window.open(url)
+})
+
+btnEstados.addEventListener('click', function(){
+    let url = 'https://github.com/EduardoFeitosa08/PBack---Aula-8-e-Atividade-3'
+    window.open(url)
+})
