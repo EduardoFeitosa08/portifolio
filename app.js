d1b5afffe39c851e8e15ea851130e0d0361fd176
EDUARDO FEITOSA BATISTA <25132675@CFP127ALUNO.LOCAL>
2025-10-30 16:12:01 -0300

    README criado e Responsividade Implementada

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 'use strict'
 
 const btnRick = document.getElementById('btn_rick')
+const btnRickWiki = document.getElementById('btn_rick_wiki')
 const btnEstados = document.getElementById('btn_estados')
 
 btnRick.addEventListener('click', function(){
     let url = 'https://github.com/EduardoFeitosa08/Rick-and-Morty-Wiki'
+    window.open(url)
+})
+
+btnRickWiki.addEventListener('click', function(){
+    let url = 'https://eduardofeitosa08.github.io/Rick-and-Morty-Wiki/'
     window.open(url)
 })
 
